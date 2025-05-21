7ad99a7a9df59dc73069f6fa59d13467b3485818
daromi94 <davromero07@gmail.com>
2025-05-20 22:46:25 -0500

    feat: :sparkles: add counter actor

---
+++ src/core/src/main/scala/com/daromi/dwc/core/Counter.scala
@@ -0,0 +1,30 @@
+package com.daromi.dwc.core
+
+import org.apache.pekko.actor.typed.Behavior
+import org.apache.pekko.actor.typed.scaladsl.{
+  AbstractBehavior,
+  ActorContext,
+  Behaviors
+}
+
+sealed trait CounterMessage
+
+final class Counter(
+    context: ActorContext[CounterMessage]
+) extends AbstractBehavior[CounterMessage](context):
+
+  var count: Int = 0
+
+  override def onMessage(msg: CounterMessage): Behavior[CounterMessage] =
+    msg match
+      case Counter.Increment =>
+        count += 1
+        println(count)
+        this
+
+object Counter:
+
+  case object Increment extends CounterMessage
+
+  def apply(): Behavior[CounterMessage] =
+    Behaviors.setup(context => new Counter(context))
